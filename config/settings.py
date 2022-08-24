ffc2d0ec25ee3f8e9fbba643b68b908890a54d68
Mexrojxon <mexrojxon.dev@gmail.com>
2022-08-24 16:21:47 +0500

    static fayllar qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -25,6 +25,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'about',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -101,9 +104,17 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static'
+
+STATICFILES_DIRS = BASE_DIR / 'assets',
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
