fc567d71f6e46495ce17fcfce80f1fdf53819b58
Mexrojxon <mexrojxon.dev@gmail.com>
2022-08-25 16:46:46 +0500

    Shablon xatoliklari tuzatildi va resume uchun model yozildi

--- about/models.py
+++ about/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.db.models import FilePathField
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class ResumeModel(models.Model):
+    resume = models.FileField(verbose_name=_('resume'), upload_to="resume/",)
+
+    def __name__(self):
+        return self.resume
