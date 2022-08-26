eb9b0dbea071ec9cb7e6cdd7965b552d19230c12
Mexrojxon <mexrojxon.dev@gmail.com>
2022-08-26 11:54:44 +0500

    Education model yozildi va contextda intefacega yuborildi

--- about/models.py
+++ about/models.py
@@ -4,7 +4,24 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ResumeModel(models.Model):
-    resume = models.FileField(verbose_name=_('resume'), upload_to="resume/",)
+    resume = models.FileField(verbose_name=_('resume'), upload_to="resume/", )
 
     def __name__(self):
         return self.resume
+
+
+class EducationModel(models.Model):
+    place = models.CharField(max_length=100, verbose_name=_('place'))
+    country = models.CharField(max_length=50, verbose_name=_('country'))
+    faculty = models.CharField(max_length=70, verbose_name=_('faculty'), blank=True, null=True)
+    start_date = models.DateField(verbose_name=_('start_date'))
+    end_date = models.DateField(verbose_name=_('end_date'))
+    short_description = models.CharField(max_length=255, verbose_name=_('short_description'))
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created_at'))
+
+    class Meta:
+        verbose_name = _('education')
+        verbose_name_plural = _('educations')
+
+    def __str__(self):
+        return self.place
--- about/views.py
+++ about/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from .models import EducationModel
 
 class MainPageView(TemplateView):
     template_name = 'about/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainPageView, self, **kwargs).get_context_data()
+        context['education'] = EducationModel.objects.all()
+
+        return context
