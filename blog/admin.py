3c84e308632b9f2491965425949851d7709a5aa7
Mexrojxon <mexrojxon.dev@gmail.com>
2022-08-27 22:14:01 +0500

    Ckeditor o'rnatildi va blog modelga qo'shildi

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
+admin.site.register(CategoryModel)
+admin.site.register(TagModel)
+admin.site.register(BlogPostModel)
 
-# Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class CategoryModel(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-class BlogTagModel(models.Model):
+class TagModel(models.Model):
     title = models.CharField(max_length=100, verbose_name=_('title'))
     created_at = models.DateTimeField(verbose_name=_('created_at'))
     def __str__(self):
@@ -28,7 +28,9 @@ class BlogTagModel(models.Model):
 class BlogPostModel(models.Model):
     title = models.CharField(max_length=300, verbose_name=_('title'))
     description = RichTextUploadingField(null = True, verbose_name=_('description'))
-    tag = models.ManyToManyField(BlogTagModel, related_name='post', verbose_name=_('tags'))
+    image = models.ImageField(upload_to='blog/', verbose_name=_('image'))
+    category = models.ForeignKey(CategoryModel, on_delete=models.RESTRICT, related_name='post')
+    tag = models.ManyToManyField(TagModel, related_name='post', verbose_name=_('tags'))
     created_at = models.DateTimeField(auto_now_add=True, verbose_name=_('created'))
 
     def __str__(self):
