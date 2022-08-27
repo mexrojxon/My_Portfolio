0b4073f9675364438e353edc185d7b987cddddc1
Mexrojxon <mexrojxon.dev@gmail.com>
2022-08-27 22:42:55 +0500

    Post contextda interfacega yuborildi

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, ListView
 from about.models import SocialMediaModel
+from blog.models import BlogPostModel
+
 
 class BlogListView(TemplateView):
     template_name = 'blog/blog.html'
 
     def get_context_data(self, **kwargs):
         context = super(BlogListView, self, **kwargs).get_context_data()
+        context['post'] = BlogPostModel.objects.all().order_by('-id')
         context['social'] = SocialMediaModel.objects.all()
 
         return context
